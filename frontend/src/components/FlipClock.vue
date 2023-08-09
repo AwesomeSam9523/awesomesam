1934fa036f153f33d1b450082ce32a3cdd6c40fc
AwesomeSam9523 <samakshgupta04@gmail.com>
2023-08-09 17:20:35 +0530

    stuff

---
+++ frontend/src/components/FlipClock.vue
@@ -0,0 +1,208 @@
+<template>
+  <div class="flip-clock" ref="clock">
+    <span class="flip-clock__piece flip" ref="DaysTracker">
+      <b class="flip-clock__card card">
+        <b class="card__top">11</b>
+        <b class="card__bottom" data-value="12"></b>
+        <b class="card__back" data-value="12">
+          <b class="card__bottom" data-value="11"></b>
+        </b>
+      </b>
+      <span class="flip-clock__slot">Days</span>
+    </span>
+    <span class="flip-clock__piece flip">
+      <b class="flip-clock__card card">
+        <b class="card__top">23</b>
+        <b class="card__bottom" data-value="00"></b>
+        <b class="card__back" data-value="00">
+          <b class="card__bottom" data-value="23"></b>
+        </b>
+      </b>
+      <span class="flip-clock__slot">Hours</span>
+    </span>
+    <span class="flip-clock__piece flip">
+      <b class="flip-clock__card card">
+        <b class="card__top">59</b>
+        <b class="card__bottom" data-value="00"></b>
+        <b class="card__back" data-value="00">
+          <b class="card__bottom" data-value="59"></b>
+        </b>
+      </b>
+      <span class="flip-clock__slot">Minutes</span>
+    </span>
+    <span class="flip-clock__piece flip">
+      <b class="flip-clock__card card">
+        <b class="card__top">09</b>
+        <b class="card__bottom" data-value="10"></b>
+        <b class="card__back" data-value="10">
+          <b class="card__bottom" data-value="09"></b>
+        </b>
+      </b>
+      <span class="flip-clock__slot">Seconds</span>
+    </span>
+  </div>
+</template>
+
+<script>
+import { Clock } from "@/assets/scripts/clock.ts"
+
+export default {
+  mounted() {
+    var deadline = new Date(Date.parse(new Date()) + 12 * 24 * 60 * 60 * 1000);
+    var c = new Clock(deadline, function () { alert('countdown complete') }, this.$refs);
+    // document.body.appendChild(c.el);
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="less">
+html {
+  height: 100%;
+}
+
+body {
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #EEE;
+}
+
+.flip-clock {
+  display: flex;
+  flex-direction: row;
+  text-align: center;
+  perspective: 400px;
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+}
+
+.flip-clock__piece {
+  display: flex;
+  flex-direction: column;
+  margin: 0 0.75rem;
+}
+
+.flip-clock__slot {
+  font-size: 2vw;
+}
+
+@halfHeight: 0.72em;
+@borderRadius: 0.15em;
+
+.card {
+  display: block;
+  position: relative;
+  padding-bottom: @halfHeight;
+  font-size: 9vw;
+  line-height: 0.95;
+}
+
+.card__top,
+.card__bottom,
+.card__back::before,
+.card__back::after {
+  display: block;
+  height: @halfHeight;
+  color: #ccc;
+  background: #222;
+  padding: 0.25em 0.25em;
+  border-radius: @borderRadius @borderRadius 0 0;
+  backface-visiblity: hidden;
+  transform-style: preserve-3d;
+  width: 1.8em;
+  transform: translateZ(0);
+}
+
+.card__bottom {
+  color: #FFF;
+  position: absolute;
+  top: 50%;
+  left: 0;
+  border-top: solid 1px #000;
+  background: #393939;
+  border-radius: 0 0 @borderRadius @borderRadius;
+  pointer-events: none;
+  overflow: hidden;
+}
+
+.card__bottom::after {
+  display: block;
+  margin-top: -@halfHeight;
+}
+
+.card__back::before,
+.card__bottom::after {
+  content: attr(data-value);
+}
+
+.card__back {
+  position: absolute;
+  top: 0;
+  height: 100%;
+  left: 0%;
+  pointer-events: none;
+}
+
+.card__back::before {
+  position: relative;
+  z-index: -1;
+  overflow: hidden;
+}
+
+.flip .card__back::before {
+  animation: flipTop 0.3s cubic-bezier(.37, .01, .94, .35);
+  animation-fill-mode: both;
+  transform-origin: center bottom;
+}
+
+.flip .card__back .card__bottom {
+  transform-origin: center top;
+  animation-fill-mode: both;
+  animation: flipBottom 0.6s cubic-bezier(.15, .45, .28, 1); // 0.3s; 
+}
+
+@keyframes flipTop {
+  0% {
+    transform: rotateX(0deg);
+    z-index: 2;
+  }
+
+  0%,
+  99% {
+    opacity: 0.99;
+  }
+
+  100% {
+    transform: rotateX(-90deg);
+    opacity: 0;
+  }
+}
+
+@keyframes flipBottom {
+
+  0%,
+  50% {
+    z-index: -1;
+    transform: rotateX(90deg);
+    opacity: 0;
+  }
+
+  51% {
+    opacity: 0.99;
+  }
+
+  100% {
+    opacity: 0.99;
+    transform: rotateX(0deg);
+    z-index: 5;
+  }
+}
+</style>
--- frontend/src/views/MujPage.vue
+++ frontend/src/views/MujPage.vue
@@ -1,31 +1,72 @@
 <template>
-  <div class="background">
-    <!-- <img :src="require('@/assets/backgrounds/MUJ.webp')"> -->
-    <section :style="{'background': 'url(' + require('@/assets/backgrounds/MUJ_night_sky.jpg') + ')'}">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </section>
-  </div>
-  <div class="container">
-    <div class="heading">
-      New Horizon
+  <div class="pages">
+    <div class="container page1">
+      <div class="background">
+        <section :style="{ 'background': 'url(' + require('@/assets/backgrounds/MUJ_night_sky.jpg') + ')' }">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </section>
+      </div>
+      <div class="heading">
+        New Horizon
+      </div>
+      <div class="tagline">
+        “A Fresher’s Fiesta”
+      </div>
+      <div class="field">
+        <div class="text" @click="scrollToPage2">Scroll</div>
+        <div class="scroll" @click="scrollToPage2"></div>
+      </div>
     </div>
-    <div class="tagline">
-      “A Fresher’s Fiesta”
+    <div class="container page2">
+      <div class="background2">
+        <!-- add particles.js -->
+        <div id="particles-js" style="opacity: 0.75;"></div>
+      </div>
+      <div class="clock" v-if="showClock">
+        <FlipClock />
+      </div>
+      <div class="links-cont" v-else>
+        
+      </div>
     </div>
   </div>
 </template>
   
 <script>
+import '@/assets/scripts/particles.js';
+import config from '@/assets/scripts/config.js';
+import FlipClock from '@/components/FlipClock.vue';
+
+
 export default {
+  components: {
+    FlipClock
+  },
+  data() {
+    return {
+      showClock: true,
+    }
+  },
+  mounted() {
+    window.particlesJS('particles-js', config);
+  },
+  methods: {
+    scrollToPage2() {
+      window.scrollTo({
+        top: window.innerHeight,
+        behavior: 'smooth'
+      })
+    }
+  }
 }
 </script>
 
@@ -33,9 +74,16 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Borel&family=Patua+One&display=swap');
 
 .background {
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+
+.background2 {
+  background-color: black;
   width: 100%;
   height: 100%;
   z-index: -1;
@@ -49,8 +97,8 @@ export default {
 }
 
 .container {
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -58,7 +106,6 @@ export default {
 }
 
 .heading {
-  margin-top: 8rem;
   display: flex;
   font-family: 'Patua One', sans-serif;
   font-size: 7rem;
@@ -74,7 +121,7 @@ export default {
 }
 
 body {
-  overflow: hidden;
+  overflow-x: hidden;
 }
 
 section {
@@ -87,11 +134,13 @@ section {
   // background: url("https://i.postimg.cc/c1Q3njM0/bg2.jpg");
   /*   background-color:black; */
   background-size: cover !important;
-  animation: backgroundAnimation 50s linear infinite;
+  background-position: center !important;
+  // animation: backgroundAnimation 50s linear infinite;
   z-index: -1;
 }
 
 @keyframes backgroundAnimation {
+
   0%,
   100% {
     transform: scale(1);
@@ -223,6 +272,104 @@ span:nth-child(9) {
 @media (max-width: 768px) {
   .heading {
     font-size: 5rem;
+  }
 }
-}</style>
+.field {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 16rem;
+  flex-direction: column;
+  // width: 300px;
+}
+.text {
+  font-family: 'Arial';
+  cursor: pointer;
+  color: white;
+  font-size: 1.2rem;
+  margin-bottom: 0.5rem;
+  text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #01e1ff, 0 0 70px #01e1ff, 0 0 80px #01e1ff, 0 0 100px #01e1ff, 0 0 150px #01e1ff;
+}
+.scroll {
+  width: 60px;
+  height: 60px;
+  border: 2px solid #01e1ff;
+  border-radius: 50%;
+  position: relative;
+  animation: down 1.5s infinite;
+  -webkit-animation: down 1.5s infinite;
+  cursor: pointer;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 15px;
+    left: 18px;
+    width: 18px;
+    height: 18px;
+    border-left: 2px solid #dcdcdc;
+    border-bottom: 2px solid #dcdcdc;
+    transform: rotate(-45deg);
+  }
+}
+
+@keyframes down {
+  0% {
+    transform: translate(0);
+  }
+
+  20% {
+    transform: translateY(15px);
+  }
+
+  40% {
+    transform: translate(0);
+  }
+}
+
+@-webkit-keyframes down {
+  0% {
+    transform: translate(0);
+  }
+
+  20% {
+    transform: translateY(15px);
+  }
+
+  40% {
+    transform: translate(0);
+  }
+}
+.pages {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  overflow: hidden;
+  flex-wrap: wrap;
+}
+.page1 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.page2 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  // background-color: #000000;
+}
+.links-cont {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
   
