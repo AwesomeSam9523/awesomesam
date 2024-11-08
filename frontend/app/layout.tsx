09b656fd754b3b9d3805d66be64cf8f49b469269
AwesomeSam9523 <samakshgupta04@gmail.com>
2024-11-09 01:02:21 +0530

    Add preconnect

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -16,6 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel={"preconnect"} href="https://fonts.gstatic.com" />
+        <link rel={"preconnect"} href={"https://fonts.googleapis.com"} />
+        <title>AwesomeSam</title>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
